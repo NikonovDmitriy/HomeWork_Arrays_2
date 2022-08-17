b3c724eb0f6744d600ab6c7c19baaf8f4c788d87
nikonovdmitiy <nikonov9094@gmail.com>
2022-08-17 16:56:57 +0500

    дз

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
-    public static void main(String[] args) {
-
-
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
+
+    //Задание 1
+    public static void main(String[] args) {
+        int[] arr = generateRandomArray();
+        int arr1 = 0;
+        for (int i = 0; i < arr.length-1; i++) {
+            arr1 = arr1+arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила "+arr1+" рублей");
+    }
+
+    //Задание 2
 }
